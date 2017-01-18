dc3e22ef34f71cc0a58230b77ab79ada421fb1e7
Santo Cariotti <sancn@live.com>
2017-01-18 22:18:36 +0100

    Update numeri.cc

--- numeri.cc
+++ numeri.cc
@@ -8,11 +8,14 @@ int main(void)
 	fstream file;
 	file.open("dati.dat");
 	
-	int a, b, c, seq = 0, tot = 0, num;
-	char stringa[] = "112356812983";
+	typedef unsigned short int size_t;
+	
+	size_t a, b, c, seq = 0, tot = 0, num;
+	char stringa[] = "12312312312";
 	char l;
 	while(true) 
 	{
+		cout << "3 numeri: ";
 		cin >> a >> b >> c;
 		if(a >= 0 && a <= 9 && b >= 0 && b <= 9 && c >= 0 && c <= 9){
 			if(a != b && b != c && a != c) break;

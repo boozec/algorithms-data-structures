54c7677db1fd2c4f8372d84d0585dbcd38825ecf
Santo Cariotti <sancn@live.com>
2017-01-27 21:12:07 +0100

    Add files via upload

---
+++ substring.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void s(int i, int k, int n, char str[]);
+
+int main()
+{
+	char ss[] = "SANTO";
+	s(0, 4, sizeof(ss), ss);
+	
+	return 0;
+}
+
+void s(int i, int k, int n, char str[])
+{
+	if(i < n){
+		if(i < n-k) for(int j = i; j < i+k; j++) cout << str[j];
+		cout << "\n";
+		s(i+=1, k, n, str);
+	}
+}

ada1dc01a41785241404bc486713def52344830e
Santo Cariotti <sancn@live.com>
2017-01-28 19:42:38 +0100

    Update substring.cc

--- substring.cc
+++ substring.cc
@@ -14,7 +14,7 @@ int main()
 
 void s(int i, int k, int n, char str[])
 {
-	if(i < n){
+	if(i < n - k){
 		if(i < n-k) for(int j = i; j < i+k; j++) cout << str[j];
 		cout << "\n";
 		s(i+=1, k, n, str);

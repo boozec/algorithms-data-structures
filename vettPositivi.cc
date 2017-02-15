881ffec87c9a10b599b7aa162f02669ceb0d89bd
Santo Cariotti <sancn@live.com>
2017-02-15 16:38:30 +0100

    Update vettPositivi.cc

--- vettPositivi.cc
+++ vettPositivi.cc
@@ -2,50 +2,42 @@
 	
 /*	dato un vettore di grandezza 100, stampare i valori caricati 
 	precedentemente che sono multipli del numero 2 */
-	
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
 #include <vector>
 
 using namespace std;
-	
-int n_rand(int&);
 
 const int n = 100;
 
+int n_rand(int num)
+{
+	num = rand() % n;
+	
+	return num;
+}
+
 int main()
 {
-	srand(time(NULL));
+	srand(time(0));
 		
 	int vett1[n], n_casuale, i;
 	vector<int> vett2;
 	
 	for(i = 0; i < n; i++) {
-		n_rand(n_casuale);
-		vett1[i] = n_casuale;
+		vett1[i] = n_rand(n_casuale);
+		if((vett1[i] % 2) == 0) 
+			vett2.push_back(vett1[i]);
 	}
 	
 	cout << "Numeri del vettore: " << endl;
-	for(i = 0; i < n; i++) 
+	for(i = 0; i < n; i++)
 		cout << vett1[i] << " ";
 	
-	cout << "\n\nNumeri del vettore multipli di 2: " << endl;
-	for(i = 0; i < n; i++) 
-		if((vett1[i] % 2) == 0) 
-			vett2.push_back(vett1[i]);
-	
+	cout << "\n\nNumeri del vettore multipli di 2: " << endl;	
 	for(i = 0; i < (signed) vett2.size(); i++) 
 		cout << vett2[i] << " ";
 	
-	cout << "\n\n\nPremi INVIO per chiudere il programma";
-	cin.get();
 	return 0;
-}
-
-int n_rand(int& num)
-{
-	num = rand() % n;
-	
-	return n;
 }

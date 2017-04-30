c05a6582fb9a6bd6aa8ea462c77213b9215b470a
Santo Cariotti <sancn@live.com>
2017-04-30 19:09:13 +0200

    palindromo c++ in python

---
+++ python/palindroma.py
@@ -0,0 +1,15 @@
+def palindromo(parola, start, end):
+    if start >= end:
+        return True
+    elif parola[start] == parola[end]:
+        return palindromo(parola, start+1 , end-1)
+    else:
+        return False
+
+
+parola = input('Parola: ')
+
+if palindromo(parola, 0, len(parola)-1):
+    print('Palindroma')
+else:
+    print('Non palindroma')
